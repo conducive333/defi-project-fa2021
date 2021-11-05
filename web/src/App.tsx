c613d91ae56dcf1fd23de2d91ce8770c5287d3f5
ewc340 <edwardc1028@berkeley.edu>
2021-11-05 00:38:29 -0700

    Update assets and App.tsx

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,13 +1,8 @@
 import React from 'react';
-import { Navbar } from './components';
+import { MainPage } from './pages';
 
 export const App = () => {
   return (
-    <div className="App" style={{ alignItems: 'center' }}>
-      <Navbar />
-      <div style={{ height: '50vw', display: 'flex', flexDirection: 'column', alignItems: 'center', justifyContent: 'center' }}>
-        <h1>A lot of interesting NFTs will be located here.</h1>
-      </div>
-    </div>
+    <MainPage />
   );
 }
