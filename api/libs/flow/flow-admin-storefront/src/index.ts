1aa31f4395306df20ca0466be489a4b373ceed2c
ewc340 <edwardc1028@berkeley.edu>
2021-11-28 20:52:03 -0800

    Fix partial build for api libs

--- api/libs/flow/flow-admin-storefront/src/index.ts
+++ api/libs/flow/flow-admin-storefront/src/index.ts
@@ -1,4 +1,3 @@
-export * from './lib/guards/has-storefront.guard'
 export * from './lib/admin-storefront.service'
 export * from './lib/admin-storefront.module'
 export * from './lib/dto/admin-listing.dto'
